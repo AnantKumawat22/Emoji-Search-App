77a2ecd06036279271dfd7e29253aea8f3b5d78b
Anant Kumawat <anantkumawat22@gmail.com>
2021-09-29 14:41:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get(`https://emoji-api.com/emojis?access_key=757bf32b3bdd68f69f12a326ab72eaef61063597`).then((result) => {
+      setData(result.data);
+    });
+
+  }, []);
+
+  function searchFilter() {
+    var search = document.getElementById('inp_search').value.trim().toLowerCase();
+    var contents = document.getElementById('card_container_id').children;
+    for (var i = 0; i < contents.length; i++) {
+      var cardItem = document.getElementsByClassName("emoji_name")[i].innerText.trim().toLowerCase();
+      if (cardItem.includes(search)) {
+        contents[i].style.display = "";
+      }
+      else {
+        contents[i].style.display = "none";
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="title">Emoji Search App</h1>
+        <p className="app_desc">A simple emoji search app made in ReactJs.</p>
+        <input type="search" id="inp_search" placeholder="Search for a Emoji" onChange={searchFilter} />
+        <div className="card_container" id="card_container_id">
+          {
+            data && data.map((ele) => {
+              return (
+                <div className="card_item" title="Click to Copy">
+                  <p className="emoji_icon">{ele.character}</p>
+                  <p className="emoji_name">{ele.unicodeName}</p>
+                </div>
+              );
+            })
+          }
+
+        </div>
+      </div>
+    </>
   );
 }
 
